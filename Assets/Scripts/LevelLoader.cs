ccfc2d02792fbdc851b9e3d8a2c649215a85d425
Pedro Martins Oliveira <pedrofapen@gmail.com>
2026-03-28 16:43:58 -0300

    Adiciona som na transição
    Adiciona som de passos na transição de níveis

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine; 
 using UnityEngine.SceneManagement; 
 public class LevelLoader : MonoBehaviour 
-{ 
-	public Animator transition; 
+{
+    public bool activateSound = false;
+	public Animator transition;
+    public AudioSource footsteps;
 	public float transitionTime = 1f;
     private float trasitionEndDelay = 0.2f;         // Delay in the end to ensure the transition is finished
 
@@ -16,9 +18,11 @@ public class LevelLoader : MonoBehaviour
     IEnumerator LoadLevelCoroutine(string levelName)
     {
         Debug.Log(levelName + " carregado");
+        if(activateSound) footsteps.Play();
         transition.SetTrigger("Start");  
         yield return new WaitForSeconds(transitionTime + trasitionEndDelay);
         SceneManager.LoadScene(levelName);
+        footsteps.Stop();
     }
 
 }
