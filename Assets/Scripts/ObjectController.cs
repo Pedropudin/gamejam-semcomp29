e3032013367d281a1f9163293348af03436e7e4f
Pedro Martins Oliveira <pedrofapen@gmail.com>
2026-03-29 19:26:13 -0300

    Adiciona sistema de coleta de itens
    Sistema básico onde um item ao ser coletado aparece na tela e some da imagem

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectController : MonoBehaviour
+{
+    public string objectName;
+    public GameObject objectImage;
+    public Button objectButton;
+    public GameObject foundAnimation;
+    public Button foundButton;
+    public TextMeshProUGUI foundText;
+
+    private void Start()
+    {
+        objectImage.SetActive(true);
+        foundAnimation.SetActive(false);
+
+        objectButton.onClick.AddListener(OnObjectClick);
+        foundButton.onClick.AddListener(OnFoundClick);
+
+        foundText.text += objectName;
+    }
+
+    public void OnObjectClick()
+    {
+        objectImage.transform.position = Vector3.zero;
+        foundAnimation.SetActive(true);
+        objectButton.onClick.RemoveAllListeners();
+        objectButton.onClick.AddListener(OnFoundClick);
+    }
+
+    public void OnFoundClick()
+    {
+        foundAnimation.SetActive(false);
+        objectImage.SetActive(false);
+    }
+
+}
